f76e94e7ef6bbc2ed1c42c911f63e61d628688ac
Auto Info <Auto Info@DESKTOP-CBQUMTR>
2022-12-21 11:15:04 -0300

    Salvando projeto do curso de JDBC, SQL, PGSQL

---
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -0,0 +1,93 @@
+package pos_java_jdbc.pos_java_jdbc;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.Telefone;
+import model.Userposjava;
+
+public class TesteBancoJdbc {
+	@Test
+	public void initBanco() {
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+
+		userposjava.setNome("Gabriel teste 3");
+		userposjava.setEmail("gabriel email 2");
+
+		userPosDAO.salvar(userposjava);
+
+	}
+
+	@Test
+	public void initListar() {
+		UserPosDAO dao = new UserPosDAO();
+		try {
+			List<Userposjava> list = dao.listar();
+
+			for (Userposjava userposjava : list) {
+				System.out.println(userposjava);
+				System.out.println("---------------------------------------------");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void initBuscar() {
+		UserPosDAO dao = new UserPosDAO();
+		
+		try {
+			Userposjava userposjava = dao.buscar(4L);
+			
+			System.out.println(userposjava);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	@Test
+	public void initAtualizar () {
+		try {
+		
+			UserPosDAO dao = new UserPosDAO();
+			
+			Userposjava objetoBanco = dao.buscar(4L);
+			
+			objetoBanco.setNome("Nome alterado");
+			
+			dao.atualizar(objetoBanco);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void initDeletar () {
+		
+		try {
+			
+			UserPosDAO dao = new UserPosDAO();
+			dao.deletar(6L);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	@Test
+	public void testeInsertTelefone() {
+		Telefone telefone = new Telefone();
+		telefone.setNumero("(43) 99927-2197");
+		telefone.setTipo("Casa");
+		telefone.setUsuario(3L);
+		
+		UserPosDAO dao = new UserPosDAO();
+		dao.salvarTelefone(telefone);
+	}
+
+}
