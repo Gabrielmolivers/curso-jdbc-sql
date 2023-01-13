e67c5a55f4d2ed606264746f0b4bf8e519cab7a6
Auto Info <Auto Info@DESKTOP-CBQUMTR>
2023-01-13 14:35:22 -0300

    Commit

--- enviando-email/src/test/java/enviando/email/AppTest.java
+++ enviando-email/src/test/java/enviando/email/AppTest.java
@@ -3,6 +3,10 @@ package enviando.email;
 
 import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
 import org.junit.Test;
 
 /**
@@ -10,13 +14,38 @@ import org.junit.Test;
  */
 public class AppTest {
 	
+	private String userName = "gabriel.medevjava@gmail.com";
+	private String userSenha = "Nov@york123";
+	
 	@Test
 	public void testeEmail () {
 		
-		Properties properties = new Properties();
-		properties.put("mail.smtp.auth", "true");
-		properties.put("mail.smtp.starttls", "true");
-		properties.put("mail.smtp.host", "smtp.gmail.com");
+		try {
+			Properties properties = new Properties();
+			properties.put("mail.smtp.auth", "true");
+			properties.put("mail.smtp.starttls", "true");
+			properties.put("mail.smtp.host", "smtp.gmail.com");
+			properties.put("mail.smtp.port", "465");
+			properties.put("mail.smtp.socketFactory.port", "465");
+			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+			
+			Session session = Session.getInstance(properties, new Authenticator() {
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(userName, userSenha);
+				}
+				
+			});
+			
+			System.out.println(session);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
 		
 	}
 	
