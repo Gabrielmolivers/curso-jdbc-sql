6a16ef0535044551aedbf08beda7bb2cd5def6c3
Auto Info <Auto Info@DESKTOP-CBQUMTR>
2023-01-27 16:50:37 -0300

    Salvando o cadastro no banco

--- curso-jsp/src/main/java/servlets/ServletUsuarioController.java
+++ curso-jsp/src/main/java/servlets/ServletUsuarioController.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.IOException;
 
+import dao.DAOUsuarioRepository;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -11,6 +12,8 @@ import model.ModelLogin;
 
 public class ServletUsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private DAOUsuarioRepository daoUsuarioRepository = new DAOUsuarioRepository();
        
     public ServletUsuarioController() {
     }
@@ -20,21 +23,33 @@ public class ServletUsuarioController extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String id = request.getParameter("id");
-		String nome = request.getParameter("nome");
-		String email = request.getParameter("email");
-		String login = request.getParameter("login");
-		String senha = request.getParameter("senha");
-		
-		ModelLogin modelLogin = new ModelLogin();
-		modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null);
-		modelLogin.setNome(nome);
-		modelLogin.setEmail(email);
-		modelLogin.setLogin(login);
-		modelLogin.setSenha(senha);
-		
-		request.setAttribute("modelLogin", modelLogin);
-		request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+		try {
+			String id = request.getParameter("id");
+			String nome = request.getParameter("nome");
+			String email = request.getParameter("email");
+			String login = request.getParameter("login");
+			String senha = request.getParameter("senha");
+			
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null);
+			modelLogin.setNome(nome);
+			modelLogin.setEmail(email);
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+			
+			daoUsuarioRepository.gravarUsuario(modelLogin);
+			
+			request.setAttribute("msg", "Operação realizada com sucesso!");
+			
+			request.setAttribute("modelLogin", modelLogin);
+			request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
+			request.setAttribute("msg", e.getMessage());
+			redirecionar.forward(request, response);
+		}
 		
 	}
 
--- curso-jsp/src/main/webapp/principal/usuario.jsp
+++ curso-jsp/src/main/webapp/principal/usuario.jsp
@@ -80,6 +80,7 @@
 													</div>
 												</div>
 											</div>
+											<span>${msg}</span>
 										</div>
 									</div>
 								</div>
